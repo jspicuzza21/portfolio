9d3efd8d0c75c35414ebe163ed52770abee625df
Joe Spicuzza <spicuzza21@gmail.com>
2020-10-22 22:58:52 -0400

    styleing

--- client/index.js
+++ client/index.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/home';
 
 class App extends Component{  
   render(){
     return (
-        <HashRouter >
+        <BrowserRouter >
             <Switch>
               <Route exact path={'/'} component ={Home}></Route>
               <Redirect to='/' />
             </Switch>
-        </HashRouter >
+        </BrowserRouter >
     )
   }
 }

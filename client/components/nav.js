b50b7b480ada1186e7589c5e92f1411850fd39ac
Joe Spicuzza <spicuzza21@gmail.com>
2020-10-25 11:29:47 -0400

    responsive

--- client/components/nav.js
+++ client/components/nav.js
@@ -6,9 +6,9 @@ const Nav = () =>{
     <div>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <a className="navbar-brand" href="#">Joseph Spicuzza</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
-  </button>
+  </button> */}
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
       <a className="nav-link" href="#about">About</a>

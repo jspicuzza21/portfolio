5a0c0282161fa3c089259a4e9f9b3e0315522d84
jspicuzza21 <62146216+jspicuzza21@users.noreply.github.com>
2022-04-07 16:08:00 -0400

    Update projects.js

--- client/components/projects.js
+++ client/components/projects.js
@@ -6,12 +6,27 @@ import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
 const Projects = () => (
   <div id="projects">
     <h1>Projects</h1>
+      <div className='pr pr-dark'>
+      <div className='pr-inner'>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/YS6rlmG6YjY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>       
+        </div>
+        <div className='pr-description'>
+          <h3>DriveWayze <em>(Still in development)</em></h3>
+          <p>P2P online marketplace that allows users to rent and sell parking spaces from each other. Built solo. </p>
+          <p>Tech Used: React | Redux | Postgres | Sequelize | Node.js | Stripe Connect | Google Maps API | socket.io | AWS S3 | Bootstrap | Webpack | Babel | Heroku | moment.js</p>
+          <div className='pr-icons'>
+            <a href="https://drivewayze.herokuapp.com/#/"> <FontAwesomeIcon icon={faEye} size="3x" className="icon"/></a>
+          </div>
+        </div>
+      </div>
+    </div>
     <div className='pr'>
       <div className='pr-inner'>
         <div className='pr-description'>
           <h3>Smooth Jazz</h3>
           <p>A custom built e-commerce site that sells the smoothest of jazz instruments.</p>
-          <p>Tech Used: Node.js | React | React-Redux | Email-js | Webpack | Sequelize | ChakraUI | Stripe</p>
+          <p>Tech Used: Node.js | React | React-Redux | Email-js | Webpack | Sequelize | ChakraUI | Stripe | Heroku</p>
           <div className='pr-icons'>
             <a href="https://github.com/Rebel-Alliance-2004/grace-shopper"> <FontAwesomeIcon icon={faGithub} size="3x" className="icon" /></a>
             <a href="http://graces-hopper.herokuapp.com/"> <FontAwesomeIcon icon={faEye} size="3x" className="icon"/></a>
@@ -29,8 +44,8 @@ const Projects = () => (
         </div>
         <div className='pr-description'>
           <h3>SCPO Cyber Portal</h3>
-          <p>Full stack web portal built to help manage and track requests made to the Cyber Crimes Unit at the Somerset County Prosecutor's Office for forensic exams.</p>
-          <p>Tech Used: React | Redux | Postgres | Sequelize | Node.js | Email.js | Firebase | Bulma | ChakraUI | Webpack | Babel</p>
+          <p>Full stack web portal built to help manage and track requests made to the Cyber Crimes Unit at the Somerset County Prosecutor's Office for forensic exams. Built solo.</p>
+          <p>Tech Used: React | Redux | Postgres | Sequelize | Node.js | Email.js | Firebase | Bulma | ChakraUI | Webpack | Babel | Heroku</p>
           <div className='pr-icons'>
             <a href="https://scpo.herokuapp.com/"> <FontAwesomeIcon icon={faEye} size="3x" className="icon"/></a>
           </div>
@@ -41,8 +56,8 @@ const Projects = () => (
       <div className='pr-inner'>
         <div className='pr-description'>
           <h3>Bug Out</h3>
-          <p>Full stack application that makes practicing coding fun by allowing multiple users to face off in a multiplayer code-off. Each user competes to be the first one to solve coding prompts while earning power ups along the way to slow down the competition.</p>
-          <p>Tech Used: Node.js | React | Redux | React-Redux | Webpack | Babel | Postgres | Sequelize | Socket.io | ChakraUI</p>
+          <p>Full stack application that makes practicing coding fun by allowing multiple users to face off in a multiplayer code-off. Each user competes to be the first one to solve coding prompts while earning power ups along the way to slow down the competition. Built as a team.</p>
+          <p>Tech Used: Node.js | React | Redux | React-Redux | Webpack | Babel | Postgres | Sequelize | Heroku | Socket.io | ChakraUI</p>
           <div className='pr-icons'>
             <a href="https://github.com/Ice-Code-Creamery/Bug-Out"> <FontAwesomeIcon icon={faGithub} size="3x" className="icon" /></a>
             <a href="https://bugoutbrx.herokuapp.com/"> <FontAwesomeIcon icon={faEye} size="3x" className="icon"/></a>

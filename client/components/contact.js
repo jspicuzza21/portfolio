edcd6330639892d143ace634a4455b92e1857272
Joe Spicuzza <joespicuzza@Joes-MacBook-Pro.local>
2024-04-15 21:08:31 -0400

    updated resume

--- client/components/contact.js
+++ client/components/contact.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileAlt, faAt } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileAlt, faAt } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-const Contact = ()=> (
-  <div id='contact'>
+const Contact = () => (
+  <div id="contact">
     <h3>Want to connect? Click the links below to reach out!</h3>
     <div className="contact-container">
       <div className="contact">
-        <a href="https://drive.google.com/file/d/1-2vq4g749-9C44oQTuVoDZ15JEE7fkSv/view?usp=sharing"> <FontAwesomeIcon icon={faFileAlt} size="3x" className="icon"/></a>
-        <a href="mailto:j.spicuzzajr@gmail.com"> <FontAwesomeIcon icon={faAt} size="3x" className="icon"/></a>
-        <a href="https://github.com/jspicuzza21"> <FontAwesomeIcon icon={faGithub} size="3x" className="icon"/></a>
-        <a href="https://www.linkedin.com/in/joe-s-174a1161/"> <FontAwesomeIcon icon={faLinkedin} size="3x" className="icon"/></a>
+        <a href="https://drive.google.com/file/d/1B7qyNB0KBx7Whyeaj0HSw5m5pyQE34TJ/view?usp=sharing">
+          <FontAwesomeIcon icon={faFileAlt} size="3x" className="icon" />
+        </a>
+        <a href="mailto:j.spicuzzajr@gmail.com">
+          <FontAwesomeIcon icon={faAt} size="3x" className="icon" />
+        </a>
+        <a href="https://github.com/jspicuzza21">
+          <FontAwesomeIcon icon={faGithub} size="3x" className="icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/joe-s-174a1161/">
+          <FontAwesomeIcon icon={faLinkedin} size="3x" className="icon" />
+        </a>
       </div>
     </div>
   </div>
-)
+);
 
-export default Contact
+export default Contact;
